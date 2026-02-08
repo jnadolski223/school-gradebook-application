424ee12fdade9327f57211724cc95916aa41eea5
Jakub Nadolski <j.nadolski223@gmail.com>
2026-02-08 16:16:55 +0100

    Backend init

---
+++ backend/src/test/java/pl/edu/ug/schoolgradebook/SchoolgradebookApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.ug.schoolgradebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolgradebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
