bf95f652793154564b4f04e828887da2183fb610
Jakub Nadolski <j.nadolski223@gmail.com>
2026-02-17 21:54:23 +0100

    Updated school class management and updated Postman tests

--- backend/src/main/java/pl/edu/ug/schoolgradebook/dto/schoolclass/SchoolClassRequest.java
+++ backend/src/main/java/pl/edu/ug/schoolgradebook/dto/schoolclass/SchoolClassRequest.java
@@ -2,7 +2,7 @@ package pl.edu.ug.schoolgradebook.dto.schoolclass;
 
 import java.util.UUID;
 
-public record SchoolClassCreateRequest(
+public record SchoolClassRequest(
         UUID schoolId,
         UUID homeroomTeacherId,
         String name
--- backend/src/main/java/pl/edu/ug/schoolgradebook/service/SchoolClassService.java
+++ backend/src/main/java/pl/edu/ug/schoolgradebook/service/SchoolClassService.java
@@ -6,103 +6,86 @@ import org.springframework.transaction.annotation.Transactional;
 import pl.edu.ug.schoolgradebook.domain.School;
 import pl.edu.ug.schoolgradebook.domain.SchoolClass;
 import pl.edu.ug.schoolgradebook.domain.SchoolMember;
-import pl.edu.ug.schoolgradebook.dto.schoolclass.SchoolClassCreateRequest;
+import pl.edu.ug.schoolgradebook.dto.schoolclass.SchoolClassRequest;
 import pl.edu.ug.schoolgradebook.dto.schoolclass.SchoolClassResponse;
 import pl.edu.ug.schoolgradebook.dto.schoolclass.SchoolClassUpdateRequest;
 import pl.edu.ug.schoolgradebook.enums.UserRole;
 import pl.edu.ug.schoolgradebook.exception.ConflictException;
-import pl.edu.ug.schoolgradebook.exception.EntityNotFoundException;
 import pl.edu.ug.schoolgradebook.repository.SchoolClassRepository;
 import pl.edu.ug.schoolgradebook.repository.SchoolMemberRepository;
 import pl.edu.ug.schoolgradebook.repository.SchoolRepository;
+import pl.edu.ug.schoolgradebook.util.mapper.SchoolClassMapper;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
-public class SchoolClassService {
-
+@Transactional(readOnly = true)
+public class SchoolClassService extends EntityService {
     private final SchoolClassRepository schoolClassRepository;
     private final SchoolRepository schoolRepository;
     private final SchoolMemberRepository schoolMemberRepository;
+    private final SchoolClassMapper mapper;
 
-    public SchoolClassResponse create(SchoolClassCreateRequest request) {
-        School school = schoolRepository
-                .findById(request.schoolId())
-                .orElseThrow(() -> new EntityNotFoundException(School.class, request.schoolId().toString()));
+    @Transactional
+    public SchoolClassResponse createSchoolClass(SchoolClassRequest request) {
+        School school = getOrThrow(schoolRepository, School.class, request.schoolId());
+        SchoolMember teacher = getOrThrow(schoolMemberRepository, SchoolMember.class, request.homeroomTeacherId());
 
-        SchoolMember homeroomTeacher = getTeacher(request.homeroomTeacherId());
+        if (teacher.getUser().getRole() != UserRole.TEACHER) {
+            throw new ConflictException("School member must have TEACHER role");
+        }
 
-        SchoolClass schoolClass = SchoolClass.builder()
-                .school(school)
-                .homeroomTeacher(homeroomTeacher)
-                .name(request.name())
-                .build();
-
-        schoolClassRepository.save(schoolClass);
-        return mapToDto(schoolClass);
+        teacher.getUser().setRole(UserRole.HOMEROOM_TEACHER);
+        SchoolClass schoolClass = mapper.mapRequestToEntity(request, school, teacher);
+        return mapper.mapEntityToResponse(schoolClassRepository.save(schoolClass));
     }
 
-    public List<SchoolClassResponse> getAll(UUID schoolId) {
-        List<SchoolClass> classes = schoolId == null
-                ? schoolClassRepository.findAll()
-                : schoolClassRepository.findBySchool_Id(schoolId);
-
-        return classes.stream().map(this::mapToDto).toList();
+    public SchoolClassResponse getSchoolClassById(UUID schoolClassId) {
+        SchoolClass schoolClass = getOrThrow(schoolClassRepository, SchoolClass.class, schoolClassId);
+        return mapper.mapEntityToResponse(schoolClass);
     }
 
-    public SchoolClassResponse getById(UUID id) {
+    public List<SchoolClassResponse> getAllSchoolClasses() {
         return schoolClassRepository
-                .findById(id)
-                .map(this::mapToDto)
-                .orElseThrow(() -> new EntityNotFoundException(SchoolClass.class, id.toString()));
+                .findAll()
+                .stream()
+                .map(mapper::mapEntityToResponse)
+                .toList();
     }
 
-    public SchoolClassResponse update(UUID id, SchoolClassUpdateRequest request) {
-        SchoolClass schoolClass = schoolClassRepository
-                .findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(SchoolClass.class, id.toString()));
+    public List<SchoolClassResponse> getAllSchoolClassesBySchoolId(UUID schoolId) {
+        return schoolClassRepository
+                .findBySchool_Id(schoolId)
+                .stream()
+                .map(mapper::mapEntityToResponse)
+                .toList();
+    }
+
+    @Transactional
+    public SchoolClassResponse updateSchoolClass(UUID schoolClassId, SchoolClassUpdateRequest request) {
+        SchoolClass schoolClass = getOrThrow(schoolClassRepository, SchoolClass.class, schoolClassId);
+        SchoolMember currentHomeroomTeacher = schoolClass.getHomeroomTeacher();
 
         if (request.name() != null) {
             schoolClass.setName(request.name());
         }
 
-        if (request.homeroomTeacherId() != null) {
-            SchoolMember teacher = getTeacher(request.homeroomTeacherId());
-            schoolClass.setHomeroomTeacher(teacher);
+        if (request.homeroomTeacherId() != null && request.homeroomTeacherId() != currentHomeroomTeacher.getUser().getId()) {
+            SchoolMember newHomeroomTeacher = getOrThrow(schoolMemberRepository, SchoolMember.class, request.homeroomTeacherId());
+            schoolClass.setHomeroomTeacher(newHomeroomTeacher);
+            newHomeroomTeacher.getUser().setRole(UserRole.HOMEROOM_TEACHER);
+            currentHomeroomTeacher.getUser().setRole(UserRole.TEACHER);
         }
 
-        schoolClassRepository.save(schoolClass);
-        return mapToDto(schoolClass);
+        return mapper.mapEntityToResponse(schoolClass);
     }
 
-    public void delete(UUID id) {
-        if (!schoolClassRepository.existsById(id)) {
-            throw new EntityNotFoundException(SchoolClass.class, id.toString());
-        }
-        schoolClassRepository.deleteById(id);
-    }
-
-    private SchoolMember getTeacher(UUID schoolMemberId) {
-        SchoolMember member = schoolMemberRepository
-                .findById(schoolMemberId)
-                .orElseThrow(() -> new EntityNotFoundException(SchoolMember.class, schoolMemberId.toString()));
-
-        if (member.getUser().getRole() != UserRole.TEACHER) {
-            throw new ConflictException("Homeroom teacher must have TEACHER role");
-        }
-
-        return member;
-    }
-
-    private SchoolClassResponse mapToDto(SchoolClass schoolClass) {
-        return new SchoolClassResponse(
-                schoolClass.getId(),
-                schoolClass.getSchool().getId(),
-                schoolClass.getHomeroomTeacher().getUserId(),
-                schoolClass.getName()
-        );
+    @Transactional
+    public void deleteSchoolClass(UUID schoolClassId) {
+        SchoolClass schoolClass = getOrThrow(schoolClassRepository, SchoolClass.class, schoolClassId);
+        schoolClass.getHomeroomTeacher().getUser().setRole(UserRole.TEACHER);
+        schoolClassRepository.delete(schoolClass);
     }
 }
